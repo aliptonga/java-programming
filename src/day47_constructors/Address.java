70c24698291381363e02253c823fb7bc476c28c9
aliptonga <aliptonga@gmail.com>
2021-05-27 16:59:27 -0700

    added Address class

---
+++ src/day47_constructors/Address.java
@@ -0,0 +1,54 @@
+package day47_constructors;
+
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country = "USA";
+
+    @Override
+    public String toString() {
+        return street + " , " + city + " , " + state + " , " + zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
