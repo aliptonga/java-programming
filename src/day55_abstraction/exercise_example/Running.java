ed8e0a1967d039c7fd093e8a914accca0f171e65
aliptonga <aliptonga@gmail.com>
2021-08-11 18:34:56 -0700

    - abstract class practice

---
+++ src/day55_abstraction/exercise_example/Running.java
@@ -0,0 +1,14 @@
+package day55_abstraction.exercise_example;
+
+public class Running extends Exercise {
+
+    @Override
+    public void perform() {
+
+    }
+
+    @Override
+    public int getCaloriesCount(int minutes) {
+        return 0;
+    }
+}
